0e95cb0d16a64c70e514fb7e2d6cdeb4378e7a79
shivamrai85 <shivam8550@gmail.com>
2024-12-24 23:14:35 +0530

    Initial Commit

---
+++ src/com/shivamrai/beans/HistoryBean.java
@@ -0,0 +1,20 @@
+package com.shivamrai.beans;
+
+public class HistoryBean extends BookingDetails {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    private String transId;
+
+    public String getTransId() {
+        return transId;
+    }
+
+    public void setTransId(String transId) {
+        this.transId = transId;
+    }
+
+}
